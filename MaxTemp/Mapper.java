37000c7906091ff9c2af328e2225209d777890bf
Roschlynn Michael D'souza <53754448+roschlynnmichael@users.noreply.github.com>
2022-05-03 01:54:40 +0530

    Update Mapper.java

--- MaxTemp/Mapper.java
+++ MaxTemp/Mapper.java
@@ -1,21 +1,24 @@
 package hadoop_maxtemp;
 
 import java.io.IOException;
-
+import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapred.*;
 
-public class Maxtemp_mapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable>{
-	public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> values, Reporter r) throws IOException{
-		String s = value.toString();
-		String s1 = s.substring(15,19);
-		int temperature;
-		if(s.charAt(87)=='+') {
-			temperature = Integer.parseInt(s.substring(88,92));
+public class Maxtemp_mapper extends org.apache.hadoop.mapreduce.Mapper<LongWritable, Text, Text, IntWritable> {
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
+		String line = value.toString();
+		String year = line.substring(15,19);
+		int airtemp;
+		if(line.charAt(87) == '+') {
+			airtemp = Integer.parseInt(line.substring(88, 92));
 		}
-		else {
-			temperature=Integer.parseInt(s.substring(87,92));
+		else
+		{
+			airtemp = Integer.parseInt(line.substring(87, 92));
 		}
-		values.collect(new Text(s1), new IntWritable(temperature));
+		String q = line.substring(92, 93);
+		if(airtemp!=9999 && q.matches("[01459]")) {
+			context.write(new Text(year), new IntWritable(airtemp));
+		}
 	}
 }
