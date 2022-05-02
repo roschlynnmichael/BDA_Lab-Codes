f6f243bac3f7790d3780a4b5be89e6e9439b6591
Roschlynn Michael D'souza <53754448+roschlynnmichael@users.noreply.github.com>
2022-05-03 01:55:02 +0530

    Update Reducer.java

--- MaxTemp/Reducer.java
+++ MaxTemp/Reducer.java
@@ -1,17 +1,14 @@
 package hadoop_maxtemp;
 
-import java.util.Iterator;
 import java.io.IOException;
-
 import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapred.*;
 
-public class Maxtemp_reducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable>{
-	public void reduce(Text key, Iterator<IntWritable> value1, OutputCollector<Text, IntWritable> values, Reporter r) throws IOException{
-		int maxvalue=Integer.MIN_VALUE;
-		while(value1.hasNext()) {
-			maxvalue=Math.max(maxvalue, value1.next().get());
+public class Maxtemp_reducer extends org.apache.hadoop.mapreduce.Reducer<Text, IntWritable, Text, IntWritable>{
+	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException{
+		int maxvalue = Integer.MIN_VALUE;
+		for(IntWritable value : values) {
+			maxvalue = Math.max(maxvalue, value.get());
 		}
-		values.collect(key, new IntWritable(maxvalue));
+		context.write(key, new IntWritable(maxvalue));
 	}
 }
